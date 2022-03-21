3c06c02f93f3d7b7c6b70865c056e8f64a7c55b5
LorenzoAriatta <lorenzotk77@gmail.com>
2022-03-21 15:24:25 +0100

    creato array con oggetti, provo a creare elemento

--- js.js
+++ js.js
@@ -10,6 +10,12 @@ console.log('JS OK!');
 
 // -------------------- DOM ELEMENTS -------------------- //
 
+//team container
+const teamContainer = document.querySelector('team-container');
+//team card
+const teamCard = document.querySelector('team-card');
+//card text
+const cardText = document.querySelector('card-text');
 
 // -------------------- / DOM ELEMENTS -------------------- //
 
@@ -17,8 +23,44 @@ console.log('JS OK!');
 
 // -------------------- GENERAL -------------------- //
 
+// array di oggetti dei team members
 const teamMembers = [
-    { nome: }
+    {
+        name: 'Wayne Barnett',
+        role: 'Founders & CEO',
+        imgUrl: 'file:///C:/Users/loren/OneDrive/Documenti/Boolean/Boolean%20Projects/js-our-team/img/wayne-barnett-founder-ceo.jpg'
+    },
+
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        imgUrl: 'img/angela-caroll-chief-editor.jpg'
+    },
+
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        imgUrl: 'file:///C:/Users/loren/OneDrive/Documenti/Boolean/Boolean%20Projects/js-our-team/img/walter-gordon-office-manager.jpg'
+    },
+
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        imgUrl: 'file:///C:/Users/loren/OneDrive/Documenti/Boolean/Boolean%20Projects/js-our-team/img/angela-lopez-social-media-manager.jpg'
+    },
+
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        imgUrl: 'file:///C:/Users/loren/OneDrive/Documenti/Boolean/Boolean%20Projects/js-our-team/img/scott-estrada-developer.jpg'
+    },
+
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        imgUrl: 'file:///C:/Users/loren/OneDrive/Documenti/Boolean/Boolean%20Projects/js-our-team/img/barbara-ramos-graphic-designer.jpg'
+    }
 ];
+
 
 // -------------------- / GENERAL -------------------- // 
